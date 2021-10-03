47af5e97db2693336a7cf696d3222f589ff169ca
Saran <saran.1902210@srec.ac.in>
2021-10-03 14:31:48 +0530

    Done some changes for VR conversion

---
+++ Assets/Script/triggerinfo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class triggerinfo : MonoBehaviour
+{
+      public GameObject UIText;
+    
+   
+    
+    void Start()
+    {
+        //interact.transform.position = this.gameObject.transform.position;
+        UIText.SetActive(false);
+  
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") 
+        {
+            //interact.SetActive(false);
+            
+                
+                UIText.SetActive(true);
+                
+                StartCoroutine("disableinfo");
+            
+
+        }
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+            UIText.SetActive(false);
+        }
+        
+    }
+    IEnumerator disableinfo()
+    {
+        yield return new WaitForSeconds(10);
+        UIText.SetActive(false);
+        
+    }
+}
+
